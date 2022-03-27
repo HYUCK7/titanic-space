a35b3f91f1849a16e798e65372a7636a0427eed4
HYUCK7 <iqeq12191219@gmail.com>
2022-03-27 11:36:41 +0900

    2022-03-27 11:36 1

--- spacetitanic/models.py
+++ spacetitanic/models.py
@@ -1,3 +1,4 @@
+
 import context.models
 from context import models, domain
 from context.domain import Dataset
@@ -10,13 +11,80 @@ class TitanicModel:
     dataset = Dataset()
 
     def __init__(self, train_fname, test_fname):
-        self.train = self.model.new_model(train_fname)
-        self.test = self.model.new_model(test_fname)
-        ic(f'트레인 컬럼 {self.train.columns}')
-
-    def preprocess(self):
         this = self.dataset
         that = self.model
-        feature = ['PassengeId', 'HomePlanet', 'CryoSleep', 'Cabin', 'Destination', 'Age', 'VIP',
-                   'RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck', 'Name',
-                   'Transported']
+
+        self.train = self.model.new_model(train_fname)
+        self.test = self.model.new_model(test_fname)
+        # ic(f'트레인 타입 {type(self.train)}')
+        # ic(f'트레인 컬럼 {self.train.columns}')
+        # ic(f'트레인 헤드 {self.train.head()}')
+
+        this.train = that.new_dframe(train_fname)
+        this.test = that.new_dframe(test_fname)
+        this.label = this.train['Transported']  # 실제값
+        ic(f'라벨 확인{this.label}')
+        this.id = this.test['PassengerId']
+        ic(f'아이디 확인{this.id}')
+        this.train = this.train.drop('Transported', axis=1)
+        ic(f'중간 확인{this.train}')
+
+
+    def preprocess(self, train_fname, test_fname):
+        pass
+    # __init__에서 해보고 옮기기
+
+    @staticmethod
+    def drop_feature(self):
+        pass
+
+    @staticmethod
+    def HomePlanet():
+        pass
+
+    @staticmethod
+    def CryoSleep():
+        pass
+
+    @staticmethod
+    def Cabin():
+        pass
+
+    @staticmethod
+    def Destination():
+        pass
+
+    @staticmethod
+    def Age():
+        pass
+
+    @staticmethod
+    def VIP():
+        pass
+
+    @staticmethod
+    def RoomService():
+        pass
+
+    @staticmethod
+    def FoodCourt():
+        pass
+
+    @staticmethod
+    def ShoppingMall():
+        pass
+
+    @staticmethod
+    def Spa():
+        pass
+
+    @staticmethod
+    def VRDeck():
+        pass
+
+    @staticmethod
+    def Name():
+        pass
+    
+
+
